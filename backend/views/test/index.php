2180a774b5fd16bd72e9e4c15db51fe9e0e98519
aobai <aobai@ao.com>
2017-11-05 14:56:12 +0800

    11.5

---
+++ backend/views/test/index.php
@@ -0,0 +1,33 @@
+<?php
+use yii\web\JsExpression;
+
+//外部TAG
+echo \yii\bootstrap\Html::fileInput('test', NULL, ['id' => 'test']);
+echo \flyok666\uploadifive\Uploadifive::widget([
+    'url' => yii\helpers\Url::to(['s-upload']),
+    'id' => 'test',
+    'csrf' => true,
+    'renderTag' => false,
+    'jsOptions' => [
+        'formData'=>['someKey' => 'someValue'],
+        'width' => 120,
+        'height' => 40,
+        'onError' => new JsExpression(<<<EOF
+function(file, errorCode, errorMsg, errorString) {
+    console.log('The file ' + file.name + ' could not be uploaded: ' + errorString + errorCode + errorMsg);
+}
+EOF
+        ),
+        'onUploadComplete' => new JsExpression(<<<EOF
+function(file, data, response) {
+    data = JSON.parse(data);
+    if (data.error) {
+        console.log(data.msg);
+    } else {
+        console.log(data.fileUrl);
+    }
+}
+EOF
+        ),
+    ]
+]);
